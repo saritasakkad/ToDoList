fddc57a85dc8a5ad92f859b6d56faf167c8d294d
saritasakkad <akkadsaritas@gmail.com>
2017-11-07 17:10:59 +0100

    v.1.0

---
+++ todolist/script.js
@@ -0,0 +1,17 @@
+$(function(){
+
+var info
+info = {
+              "user" : "Akkad",
+              "date" : "Le 06 11 2017",
+              "task" : []
+
+}
+
+
+document.getElementById("user").innerHTML += info.user;
+document.getElementById("date").innerHTML = info.date;
+// ou document.getElementById("user").innerHTML = person.user + person.date;
+
+
+});
